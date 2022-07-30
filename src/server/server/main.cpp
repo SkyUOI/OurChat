99788ce5ae74601b7a7bc0f8fc90fb3f2bc9ef8d
李沐阳 <limuyang202011@163.com>
2022-07-30 23:50:14 +0800

    feat(client):新增ui界面

--- src/server/server/main.cpp
+++ src/server/server/main.cpp
@@ -1,5 +1,11 @@
+#include <cstdio>
+#include <base/socket.h>
 
 int main(int argc, char *argv[])
 {
-
+    printf("OurChat Server starting...\n");
+    printf("Your IP address is %s\n", socket_lib::get_self_ip());
+    for(;;) {
+        
+    }
 }
