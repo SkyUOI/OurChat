2adbc0f151a52c7419ce82ff3d2643fbec9017ab
limuy <limuyang202011@163.com>
2024-07-26 00:45:53 +0000

    ci:fmt rust code

--- script/rebuild_dev_container.py
+++ script/rebuild_dev_container.py
@@ -1,4 +1,4 @@
 import os
 
 os.chdir("..")
-os.system("docker build -f Dockerfile.dev -t ourchat_develop:latest .")
+_ = os.system("docker build -f Dockerfile.dev -t ourchat_develop:latest .")
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,15 +2,12 @@ mod cfg;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-#[command(
-    author = "SkyUOI",
-    about = "The Server of OurChat",
-)]
+#[command(author = "SkyUOI", about = "The Server of OurChat")]
 struct ArgsParser {
     #[arg(short, long, default_value_t = cfg::DEFAULT_PORT)]
-    port:usize,
+    port: usize,
     #[arg(long, default_value_t = String::from(cfg::DEFAULT_IP))]
-    ip: String
+    ip: String,
 }
 
 pub async fn lib_main() -> anyhow::Result<()> {
