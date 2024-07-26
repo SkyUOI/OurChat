4ccef593c2788e50e9b15b7b29070f173e7c55b4
limuy <limuyang202011@163.com>
2024-07-27 00:04:14 +0800

    ci:format python code

--- client/pc/uiSystem.py
+++ client/pc/uiSystem.py
@@ -1,30 +1,31 @@
-from PyQt5.QtWidgets import QApplication,QMainWindow
-from PyQt5.QtCore import QCoreApplication,Qt
-from qt_material import apply_stylesheet,list_themes
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+from qt_material import apply_stylesheet
 from ui_logic.main import Ui_Main
 import sys
 
+
 class UISystem:
-    def __init__(self,argv):
+    def __init__(self, argv):
         QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
         QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-        QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.Round)
+        QApplication.setHighDpiScaleFactorRoundingPolicy(
+            Qt.HighDpiScaleFactorRoundingPolicy.Round
+        )
         self.app = QApplication(sys.argv)
         self.mainwindow = QMainWindow()
         self.ui = None
         self.dialogs = []
         self.theme = "dark_amber.xml"
 
-        apply_stylesheet(self.app,self.theme)
-    
+        apply_stylesheet(self.app, self.theme)
 
-    def setUI(self,ui):
+    def setUI(self, ui):
         self.ui = ui(self)
         self.ui.setupUi()
-    
+
     def showUI(self):
         self.mainwindow.show()
-
 
     def exec(self):
         self.app.exec_()
@@ -35,5 +36,3 @@ if __name__ == "__main__":
     ui_system.setUI(Ui_Main)
     ui_system.showUI()
     ui_system.exec()
-    
-    
