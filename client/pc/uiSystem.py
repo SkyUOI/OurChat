0f88e19e411f431e6b8a664c9e17ea232722f7b5
senlinjun <yu2755221817@163.com>
2024-07-28 19:17:03 +0800

    other: add logo files

--- client/pc/uiSystem.py
+++ client/pc/uiSystem.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 from PyQt5.QtCore import Qt
 from qt_material import apply_stylesheet
 
@@ -13,13 +13,15 @@ class UISystem:
         self.app = QApplication(argv)
         self.mainwindow = QMainWindow()
         self.ui = None
-        self.dialogs = []
+        self.dialogs = [
+            # (dialog,ui_obj)
+        ]
         self.theme = "dark_amber.xml"
 
         apply_stylesheet(self.app, self.theme)
 
-    def setUI(self, ui):
-        self.ui = ui(self)
+    def setUI(self, ui_class):
+        self.ui = ui_class(self)
         self.ui.setupUi()
 
     def showUI(self):
@@ -27,3 +29,21 @@ class UISystem:
 
     def exec(self):
         self.app.exec_()
+
+    def setDialog(self, dialog_class, only=False):
+        new_dialog = QDialog(self.mainwindow)
+        new_dialog_ui = dialog_class(self)
+
+        if only:
+            remove_later = []
+            for index in range(len(self.dialogs)):
+                dialog, dialog_ui = self.dialogs[index]
+                if type(dialog_ui) is type(new_dialog_ui):
+                    dialog.destroy()
+                    remove_later.append(index)
+
+            for i in range(len(remove_later)):
+                self.dialogs.pop(remove_later[-1])
+                remove_later.pop(-1)
+
+        self.dialogs.append((new_dialog, new_dialog_ui))
