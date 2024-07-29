c73b921033b1442e3c3099e8f8ef4449b16e3ba0
senlinjun <yu2755221817@163.com>
2024-07-29 11:26:21 +0800

    style(client): format the code

--- client/pc/ui_logic/login.py
+++ client/pc/ui_logic/login.py
@@ -1,15 +1,16 @@
 from ui.login import Ui_Login as Ui_Login_NOLOGIC
 
+
 class Ui_Login(Ui_Login_NOLOGIC):
-    def __init__(self,uisystem,dialog):
+    def __init__(self, uisystem, dialog):
         self.uisystem = uisystem
         self.dialog = dialog
-    
+
     def setupUi(self):
         super().setupUi(self.dialog)
         self.fillText()
         self.bind()
-    
+
     def fillText(self):
         pass
 
