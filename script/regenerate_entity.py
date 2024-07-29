5026d29bb0ae57643ac7e81a6151c6fbabdf47d2
senlinjun <yu2755221817@163.com>
2024-07-29 16:57:16 +0800

    style(client): format the code

--- script/regenerate_entity.py
+++ script/regenerate_entity.py
@@ -1,4 +1,6 @@
 import os
 
 os.chdir("..")
-os.system("sea generate entity -u mysql://root:123456@localhost:3306/OurChat -o server/src/entities")
+os.system(
+    "sea generate entity -u mysql://root:123456@localhost:3306/OurChat -o server/src/entities"
+)
