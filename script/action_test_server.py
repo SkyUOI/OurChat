fd89a173644e86fb8f40896168fe9d7283fc3c94
limuy <limuyang202011@163.com>
2024-07-31 14:29:39 +0800

    ci:fix ci

--- script/action_test_server.py
+++ script/action_test_server.py
@@ -1,5 +1,8 @@
 import init_valgrind_rust
 import test_server
+import os
 
 init_valgrind_rust.init_valgrind()
+os.system("apt update")
+os.system("apt install valgrind -y")
 test_server.test_server()
