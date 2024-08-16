ffb5827bf55b644b3b26cfbf4f3f15c93a868ed8
senlinjun <yu2755221817@163.com>
2024-08-16 21:55:33 +0800

    build(client-pc): update build_pc.py

--- script/build_pc.py
+++ script/build_pc.py
@@ -1,10 +1,22 @@
 import os
 import shutil
 
+def remove_dir(path):
+    os.chdir(path)
+    for i in os.listdir():
+        if os.path.isdir(i):
+            remove_dir(i)
+        else:
+            os.remove(i)
+    os.chdir("..")
+    os.rmdir(path)
+
 os.chdir("client/pc/src")
+if "out" in os.listdir():
+    remove_dir("out")
 print("-" * 45, "Start to build", "-" * 45)
 os.system(
-    "python -m nuitka --standalone --output-dir=out --show-memory --enable-plugin=pyqt6 --remove-output --windows-console-mode=disable --windows-icon-from-ico=resources/images/logo.ico --macos-app-icon=resources/images/logo.ico --linux-icon=resources/images/logo.ico main.py"
+    "python -m nuitka --standalone --output-dir=out --show-memory --enable-plugin=pyqt6 --remove-output --windows-console-mode=disable --windows-icon-from-ico=resources/images/logo.ico --macos-app-icon=resources/images/logo.ico --linux-icon=resources/images/logo.ico --plugin-enable=upx main.py"
 )
 print("-" * 45, "Over", "-" * 45)
 print("copy resources...")
