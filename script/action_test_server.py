ed18463dd747e9c5af64d4e0778cf48c75671edc
limuy <limuyang202011@163.com>
2024-08-25 17:37:12 +0800

    ci:fix ci

--- script/action_test_server.py
+++ script/action_test_server.py
@@ -3,11 +3,8 @@
 import init_valgrind_rust
 import test_server
 import sys
-from basic import msg_system
 
 init_valgrind_rust.init_valgrind()
-msg_system("apt update")
-msg_system("apt install valgrind -y")
 if len(sys.argv) > 1:
     test_inst = sys.argv[1:]
 else:
