e59b678376fca3a5ece77dc40adbe5c4846ca01c
limuy <limuyang202011@163.com>
2024-08-27 20:51:42 +0800

    test(server):fix Future in login test

--- server/tests/test_login_register.rs
+++ server/tests/test_login_register.rs
@@ -2,7 +2,7 @@ mod test_lib;
 
 async fn test_login_register() {
     // 注册和登录会被自动运行
-    test_lib::get_connection();
+    test_lib::get_connection().await;
 }
 
 register_test!(test_login_register);
