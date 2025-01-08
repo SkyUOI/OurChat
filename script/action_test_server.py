af186817dd23ba73b6ca8c10422f25f4f497e9bd
limuy <limuyang202011@163.com>
2025-01-08 22:14:22 +0800

    ci: fix ci

--- script/action_test_server.py
+++ script/action_test_server.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-
 import init_valgrind_rust
 
 init_valgrind_rust.init_valgrind()
-os.putenv("OURCHAT_TEST_CONFIG_DIR", "config/")
-os.system("cargo test")
+os.putenv("OURCHAT_TEST_CONFIG_DIR", os.path.abspath("config/"))
+if os.system("cargo test"):
+    os._exit(1)
