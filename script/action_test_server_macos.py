4d85d1bb33d40ad17c782cae822759899493e9ac
limuy <limuyang202011@163.com>
2025-01-09 11:07:24 +0800

    ci: remove useless script

--- script/action_test_server_macos.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import init_valgrind_rust
-
-init_valgrind_rust.init_valgrind()
-os.putenv("OURCHAT_TEST_CONFIG_DIR", os.path.abspath("config/gh_test"))
-if os.system("cargo test"):
-    os._exit(1)
