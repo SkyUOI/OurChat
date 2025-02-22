26265354dd228e9c2e3113b0e9e25d3372c6404a
limuy <limuyang202011@163.com>
2025-02-22 22:35:44 +0800

    feat(server): read the env var from .env file

--- server/http_server/src/main.rs
+++ server/http_server/src/main.rs
@@ -5,6 +5,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
     let mut app = Launcher::build().await?;
     app.run_forever().await?;
     Ok(())
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,6 +6,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
     let parser = ArgsParser::parse();
     let config = server::get_configuration(parser.shared_cfg.config.clone())?;
     let mut application = server::Application::build(parser, config).await?;
