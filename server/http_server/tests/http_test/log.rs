dc68f106c60bdfe4870f3c6bd05b51794c77d2ed
limuy <limuyang202011@163.com>
2025-06-01 00:58:52 +0800

    test(server): fix test ci

--- server/http_server/tests/http_test/log.rs
+++ server/http_server/tests/http_test/log.rs
@@ -13,6 +13,7 @@ async fn test_log_clear() {
     let mut config = TestHttpApp::get_config().await.unwrap();
     config.main_cfg.log_clean_duration = Duration::from_secs(1);
 
+    fs::create_dir_all(LOG_OUTPUT_DIR).unwrap();
     let path = PathBuf::from(LOG_OUTPUT_DIR).join(PathBuf::from(format!(
         "{HTTP_SERVER_LOG_PREFIX}.2020-01-01"
     )));
--- server/tests/server/log.rs
+++ server/tests/server/log.rs
@@ -13,6 +13,7 @@ async fn test_log_clear() {
     let (mut config, args) = TestApp::get_test_config().unwrap();
     config.main_cfg.log_clean_duration = Duration::from_secs(1);
 
+    fs::create_dir_all(LOG_OUTPUT_DIR).unwrap();
     let path = PathBuf::from(LOG_OUTPUT_DIR)
         .join(PathBuf::from(format!("{OURCHAT_LOG_PREFIX}.2020-01-01")));
     File::create(&path).unwrap();
