1d5b54f35cc6950f758846f37c95b3874f1cbd0a
limuy <limuyang202011@163.com>
2025-06-06 13:31:01 +0800

    feat(client): support tls encryption

--- client/lib/ourchat/ourchat_server.dart
+++ client/lib/ourchat/ourchat_server.dart
@@ -1,5 +1,6 @@
 import 'package:grpc/grpc.dart';
 import 'package:ourchat/const.dart';
+import 'package:ourchat/log.dart';
 import 'package:ourchat/service/basic/server/v1/server.pb.dart';
 import 'package:ourchat/service/basic/v1/basic.pbgrpc.dart';
 
@@ -41,12 +42,35 @@ class OurChatServer {
   ServerVersion? serverVersion;
   OurchatInterceptor? interceptor;
 
-  OurChatServer(this.host, this.port) {
+  OurChatServer(this.host, this.port, bool ssl) {
+    // try ssl/tls connection
+    if (!ssl) {
+      logger.w("Switch to insecure connection");
+    }
     channel = ClientChannel(
       host,
       port: port,
-      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+      options: ssl
+          ? const ChannelOptions(credentials: ChannelCredentials.secure())
+          : const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
+  }
+
+  static Future<bool> tlsEnabled(String host, int port) async {
+    try {
+      var channel = ClientChannel(
+        host,
+        port: port,
+        options: const ChannelOptions(credentials: ChannelCredentials.secure()),
+      );
+      var stub = BasicServiceClient(channel);
+      await stub.getServerInfo(GetServerInfoRequest());
+      return true;
+    } on GrpcError catch (e) {
+      if (e.code == unavailableStatusCode &&
+          e.message!.contains("HandshakeException")) {}
+      return false;
+    }
   }
 
   Future getServerInfo() async {
@@ -63,8 +87,10 @@ class OurChatServer {
       serverName = res.serverName;
       return okStatusCode;
     } on GrpcError catch (e) {
+      logger.e("Failed to get server info: ${e.message}");
       return e.code;
     } catch (e) {
+      logger.e("Failed to get server info: $e");
       return unknownStatusCode;
     }
   }
