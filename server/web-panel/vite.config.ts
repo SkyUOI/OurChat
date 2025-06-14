4df3e7ff87092ec4fc6babbe80bf4deaaf9e76c4
limuy <limuyang202011@163.com>
2025-06-14 16:08:07 +0800

    save

--- server/web-panel/vite.config.ts
+++ server/web-panel/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
