5952775c4b405570bdc6376762de6f8aced7b9f1
senlinjun <2755221817@qq.com>
2025-07-16 20:51:45 +0800

    feat(client): reconnect

--- client/lib/core/event.dart
+++ client/lib/core/event.dart
@@ -10,6 +10,7 @@ import 'package:ourchat/main.dart';
 import 'package:ourchat/service/ourchat/friends/accept_friend_invitation/v1/accept_friend_invitation.pb.dart';
 import 'package:ourchat/service/ourchat/msg_delivery/v1/msg_delivery.pb.dart';
 import 'package:ourchat/service/ourchat/v1/ourchat.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
 
 class OurchatEvent {
   Int64? eventId;
@@ -156,96 +157,102 @@ class OurchatEventSystem {
         interceptors: [ourchatAppState.server!.interceptor!]);
     var res = stub.fetchMsgs(FetchMsgsRequest(
         time: ourchatAppState.thisAccount!.latestMsgTime.timestamp));
-    await for (var event in res) {
-      {
-        ourchatAppState.thisAccount!.latestMsgTime =
-            OurchatTime(inputTimestamp: event.time);
-        ourchatAppState.thisAccount!.updateLatestMsgTime();
-        var row = await (ourchatAppState.privateDB!
-                .select(ourchatAppState.privateDB!.record)
-              ..where(
-                  (u) => u.eventId.equals(BigInt.from(event.msgId.toInt()))))
-            .getSingleOrNull();
-        if (row != null) {
-          // 重复事件
-          continue;
-        }
-        logger.i("receive new event(type:${event.whichRespondEventType()})");
-        // 创建一个发送者oc账号对象
-        OurchatAccount sender = OurchatAccount(ourchatAppState);
-        sender.recreateStub();
-        OurchatEvent? eventObj;
-        switch (event.whichRespondEventType()) {
-          case FetchMsgsResponse_RespondEventType // 收到好友申请
-                .newFriendInvitationNotification:
-            sender.id = event.newFriendInvitationNotification.inviterId;
-            OurchatAccount invitee = OurchatAccount(ourchatAppState);
-            invitee.id = event.newFriendInvitationNotification.inviteeId;
-            eventObj = NewFriendInvitationNotification(ourchatAppState,
-                eventId: event.msgId,
-                sender: sender,
-                sendTime: OurchatTime(inputTimestamp: event.time),
-                leaveMessage:
-                    event.newFriendInvitationNotification.leaveMessage,
-                invitee: invitee);
-            break;
-          case FetchMsgsResponse_RespondEventType // 收到好友申请结果
-                .friendInvitationResultNotification:
-            OurchatAccount invitee = OurchatAccount(ourchatAppState);
-            sender.id = event.friendInvitationResultNotification.inviterId;
-            invitee.id = event.friendInvitationResultNotification.inviteeId;
-            invitee.recreateStub();
-            List<NewFriendInvitationNotification> eventObjList =
-                await selectNewFriendInvitation();
-            List<Int64> requestEventIds = [];
-            for (int i = 0; i < eventObjList.length; i++) {
-              if ((eventObjList[i].sender!.id == sender.id &&
-                      eventObjList[i].data!["invitee"] ==
-                          ourchatAppState.thisAccount!.id.toInt()) ||
-                  eventObjList[i].sender!.id ==
-                      ourchatAppState.thisAccount!.id) {
-                eventObjList[i].data!["status"] =
-                    (event.friendInvitationResultNotification.status ==
-                            AcceptFriendInvitationResult
-                                .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
-                        ? 1
-                        : 2);
-                eventObjList[i].read = true;
-                eventObjList[i].data!["result_event_id"] = event.msgId.toInt();
-                requestEventIds.add(eventObjList[i].eventId!);
-                await eventObjList[i].saveToDB(ourchatAppState.privateDB!);
+    try {
+      await for (var event in res) {
+        {
+          ourchatAppState.thisAccount!.latestMsgTime =
+              OurchatTime(inputTimestamp: event.time);
+          ourchatAppState.thisAccount!.updateLatestMsgTime();
+          var row = await (ourchatAppState.privateDB!
+                  .select(ourchatAppState.privateDB!.record)
+                ..where(
+                    (u) => u.eventId.equals(BigInt.from(event.msgId.toInt()))))
+              .getSingleOrNull();
+          if (row != null) {
+            // 重复事件
+            continue;
+          }
+          logger.i("receive new event(type:${event.whichRespondEventType()})");
+          // 创建一个发送者oc账号对象
+          OurchatAccount sender = OurchatAccount(ourchatAppState);
+          sender.recreateStub();
+          OurchatEvent? eventObj;
+          switch (event.whichRespondEventType()) {
+            case FetchMsgsResponse_RespondEventType // 收到好友申请
+                  .newFriendInvitationNotification:
+              sender.id = event.newFriendInvitationNotification.inviterId;
+              OurchatAccount invitee = OurchatAccount(ourchatAppState);
+              invitee.id = event.newFriendInvitationNotification.inviteeId;
+              eventObj = NewFriendInvitationNotification(ourchatAppState,
+                  eventId: event.msgId,
+                  sender: sender,
+                  sendTime: OurchatTime(inputTimestamp: event.time),
+                  leaveMessage:
+                      event.newFriendInvitationNotification.leaveMessage,
+                  invitee: invitee);
+              break;
+            case FetchMsgsResponse_RespondEventType // 收到好友申请结果
+                  .friendInvitationResultNotification:
+              OurchatAccount invitee = OurchatAccount(ourchatAppState);
+              sender.id = event.friendInvitationResultNotification.inviterId;
+              invitee.id = event.friendInvitationResultNotification.inviteeId;
+              invitee.recreateStub();
+              List<NewFriendInvitationNotification> eventObjList =
+                  await selectNewFriendInvitation();
+              List<Int64> requestEventIds = [];
+              for (int i = 0; i < eventObjList.length; i++) {
+                if ((eventObjList[i].sender!.id == sender.id &&
+                        eventObjList[i].data!["invitee"] ==
+                            ourchatAppState.thisAccount!.id.toInt()) ||
+                    eventObjList[i].sender!.id ==
+                        ourchatAppState.thisAccount!.id) {
+                  eventObjList[i].data!["status"] =
+                      (event.friendInvitationResultNotification.status ==
+                              AcceptFriendInvitationResult
+                                  .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
+                          ? 1
+                          : 2);
+                  eventObjList[i].read = true;
+                  eventObjList[i].data!["result_event_id"] =
+                      event.msgId.toInt();
+                  requestEventIds.add(eventObjList[i].eventId!);
+                  await eventObjList[i].saveToDB(ourchatAppState.privateDB!);
+                }
               }
-            }
-            eventObj = FriendInvitationResultNotification(ourchatAppState,
-                eventId: event.msgId,
-                sender: sender,
-                sendTime: OurchatTime(inputTimestamp: event.time),
-                leaveMessage:
-                    event.friendInvitationResultNotification.leaveMessage,
-                invitee: invitee,
-                accept: (event.friendInvitationResultNotification.status ==
-                        AcceptFriendInvitationResult
-                            .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
-                    ? true
-                    : false),
-                requestEventIds: requestEventIds);
-            if (event.friendInvitationResultNotification.status ==
-                AcceptFriendInvitationResult
-                    .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS) {
-              ourchatAppState.thisAccount!.getAccountInfo();
-            }
-            eventObj.read = true;
+              eventObj = FriendInvitationResultNotification(ourchatAppState,
+                  eventId: event.msgId,
+                  sender: sender,
+                  sendTime: OurchatTime(inputTimestamp: event.time),
+                  leaveMessage:
+                      event.friendInvitationResultNotification.leaveMessage,
+                  invitee: invitee,
+                  accept: (event.friendInvitationResultNotification.status ==
+                          AcceptFriendInvitationResult
+                              .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
+                      ? true
+                      : false),
+                  requestEventIds: requestEventIds);
+              if (event.friendInvitationResultNotification.status ==
+                  AcceptFriendInvitationResult
+                      .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS) {
+                ourchatAppState.thisAccount!.getAccountInfo();
+              }
+              eventObj.read = true;
 
-          default:
-            break;
-        }
-        if (eventObj != null) {
-          await eventObj.saveToDB(ourchatAppState.privateDB!);
-        } else {
-          // event 没有被任何case分支处理，属于未知事件类型
-          logger.w("Unknown event type(id:${event.msgId})");
+            default:
+              break;
+          }
+          if (eventObj != null) {
+            await eventObj.saveToDB(ourchatAppState.privateDB!);
+          } else {
+            // event 没有被任何case分支处理，属于未知事件类型
+            logger.w("Unknown event type(id:${event.msgId})");
+          }
         }
       }
+    } on GrpcError {
+      logger.w("Diconnected\nTrying to reconnect");
+      listenEvents();
     }
   }
 
