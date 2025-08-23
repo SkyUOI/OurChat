2abbd67999253282d91358e5bf04d19dedafed37
senlinjun <2755221817@qq.com>
2025-08-23 21:58:00 +0800

    fix(client): reconnect when connection is lost

--- client/lib/core/event.dart
+++ client/lib/core/event.dart
@@ -244,144 +244,143 @@ class OurchatEventSystem {
   bool listening = false;
 
   void listenEvents() async {
+    stopListening();
     var stub = OurChatServiceClient(ourchatAppState.server!.channel!,
         interceptors: [ourchatAppState.server!.interceptor!]);
-    stopListening();
+
     connection = stub.fetchMsgs(FetchMsgsRequest(
         time: ourchatAppState.thisAccount!.latestMsgTime.timestamp));
     listening = true;
     logger.i("start to listen event");
-    try {
-      await for (var event in connection!) {
-        {
-          ourchatAppState.thisAccount!.latestMsgTime =
-              OurchatTime(inputTimestamp: event.time);
-          ourchatAppState.thisAccount!.updateLatestMsgTime();
-          var row = await (ourchatAppState.privateDB!
-                  .select(ourchatAppState.privateDB!.record)
-                ..where(
-                    (u) => u.eventId.equals(BigInt.from(event.msgId.toInt()))))
-              .getSingleOrNull();
-          if (row != null) {
-            // 重复事件
-            continue;
-          }
-          FetchMsgsResponse_RespondEventType eventType =
-              event.whichRespondEventType();
-          logger.i("receive new event(type:$eventType)");
-          // 创建一个发送者oc账号对象
-          OurchatAccount sender = OurchatAccount(ourchatAppState);
-          sender.recreateStub();
-          OurchatEvent? eventObj;
-          switch (eventType) {
-            case FetchMsgsResponse_RespondEventType // 收到好友申请
-                  .newFriendInvitationNotification:
-              sender.id = event.newFriendInvitationNotification.inviterId;
-              OurchatAccount invitee = OurchatAccount(ourchatAppState);
-              invitee.id = event.newFriendInvitationNotification.inviteeId;
-              eventObj = NewFriendInvitationNotification(ourchatAppState,
-                  eventId: event.msgId,
-                  sender: sender,
-                  sendTime: OurchatTime(inputTimestamp: event.time),
-                  leaveMessage:
-                      event.newFriendInvitationNotification.leaveMessage,
-                  invitee: invitee);
-              break;
-            case FetchMsgsResponse_RespondEventType // 收到好友申请结果
-                  .friendInvitationResultNotification:
-              OurchatAccount invitee = OurchatAccount(ourchatAppState);
-              sender.id = event.friendInvitationResultNotification.inviterId;
-              invitee.id = event.friendInvitationResultNotification.inviteeId;
-              invitee.recreateStub();
-              List<NewFriendInvitationNotification> eventObjList =
-                  await selectNewFriendInvitation();
-              List<Int64> requestEventIds = [];
-              for (int i = 0; i < eventObjList.length; i++) {
-                if ((eventObjList[i].sender!.id == sender.id &&
-                        eventObjList[i].data!["invitee"] ==
-                            ourchatAppState.thisAccount!.id.toInt()) ||
-                    eventObjList[i].sender!.id ==
-                        ourchatAppState.thisAccount!.id) {
-                  eventObjList[i].data!["status"] =
-                      (event.friendInvitationResultNotification.status ==
-                              AcceptFriendInvitationResult
-                                  .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
-                          ? 1
-                          : 2);
-                  eventObjList[i].read = true;
-                  eventObjList[i].data!["result_event_id"] =
-                      event.msgId.toInt();
-                  requestEventIds.add(eventObjList[i].eventId!);
-                  await eventObjList[i].saveToDB(ourchatAppState.privateDB!);
-                }
-              }
-              eventObj = FriendInvitationResultNotification(ourchatAppState,
-                  eventId: event.msgId,
-                  sender: sender,
-                  sendTime: OurchatTime(inputTimestamp: event.time),
-                  leaveMessage:
-                      event.friendInvitationResultNotification.leaveMessage,
-                  invitee: invitee,
-                  accept: (event.friendInvitationResultNotification.status ==
-                          AcceptFriendInvitationResult
-                              .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
-                      ? true
-                      : false),
-                  requestEventIds: requestEventIds);
-              if (event.friendInvitationResultNotification.status ==
-                  AcceptFriendInvitationResult
-                      .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS) {
-                ourchatAppState.thisAccount!.getAccountInfo();
-              }
-              eventObj.read = true;
-
-            case FetchMsgsResponse_RespondEventType.msg:
-              sender.id = event.msg.senderId;
-              List<OneMessage> msgs = [];
-              for (int i = 0; i < event.msg.bundleMsgs.length; i++) {
-                OneMsg oneMsg = event.msg.bundleMsgs[i]; // OneMsg 为grpc对象
-                OneMessage oneMessage = OneMessage(); // OneMessage为OurChat对象
-                if (oneMsg.text.isNotEmpty) {
-                  oneMessage.messageType = textMsg;
-                  oneMessage.text = oneMsg.text;
-                } else if (oneMsg.image.isNotEmpty) {
-                  oneMessage.messageType = imageMsg;
-                  oneMessage.imageKey = oneMsg.image;
-                }
-                msgs.add(oneMessage);
-              }
-              eventObj = BundleMsgs(ourchatAppState,
-                  eventId: event.msgId,
-                  sender: sender,
-                  session: OurchatSession(ourchatAppState, event.msg.sessionId),
-                  sendTime: OurchatTime(inputTimestamp: event.time),
-                  msgs: msgs);
-
-            default:
-              break;
-          }
-          if (eventObj != null) {
-            await eventObj.saveToDB(ourchatAppState.privateDB!);
-            if (listeners.containsKey(eventType)) {
-              // 通知对应listener
-              for (int i = 0; i < listeners[eventType].length; i++) {
-                try {
-                  listeners[eventType][i](eventObj);
-                } catch (e) {
-                  logger.w("notify listener fail: $e");
-                }
-              }
-            }
-          } else {
-            // event 没有被任何case分支处理，属于未知事件类型
-            logger.w("Unknown event type(id:${event.msgId})");
-          }
-        }
-      }
-    } catch (e) {
+    var saveConnectionStream = connection!.handleError((e) {
       if (!listening) return;
       logger.w("Disconnected\nTrying to reconnect in 3 seconds ($e)");
       Timer(Duration(seconds: 3), listenEvents);
+    });
+    await for (var event in saveConnectionStream) {
+      {
+        ourchatAppState.thisAccount!.latestMsgTime =
+            OurchatTime(inputTimestamp: event.time);
+        ourchatAppState.thisAccount!.updateLatestMsgTime();
+        var row = await (ourchatAppState.privateDB!
+                .select(ourchatAppState.privateDB!.record)
+              ..where(
+                  (u) => u.eventId.equals(BigInt.from(event.msgId.toInt()))))
+            .getSingleOrNull();
+        if (row != null) {
+          // 重复事件
+          continue;
+        }
+        FetchMsgsResponse_RespondEventType eventType =
+            event.whichRespondEventType();
+        logger.i("receive new event(type:$eventType)");
+        // 创建一个发送者oc账号对象
+        OurchatAccount sender = OurchatAccount(ourchatAppState);
+        sender.recreateStub();
+        OurchatEvent? eventObj;
+        switch (eventType) {
+          case FetchMsgsResponse_RespondEventType // 收到好友申请
+                .newFriendInvitationNotification:
+            sender.id = event.newFriendInvitationNotification.inviterId;
+            OurchatAccount invitee = OurchatAccount(ourchatAppState);
+            invitee.id = event.newFriendInvitationNotification.inviteeId;
+            eventObj = NewFriendInvitationNotification(ourchatAppState,
+                eventId: event.msgId,
+                sender: sender,
+                sendTime: OurchatTime(inputTimestamp: event.time),
+                leaveMessage:
+                    event.newFriendInvitationNotification.leaveMessage,
+                invitee: invitee);
+            break;
+          case FetchMsgsResponse_RespondEventType // 收到好友申请结果
+                .friendInvitationResultNotification:
+            OurchatAccount invitee = OurchatAccount(ourchatAppState);
+            sender.id = event.friendInvitationResultNotification.inviterId;
+            invitee.id = event.friendInvitationResultNotification.inviteeId;
+            invitee.recreateStub();
+            List<NewFriendInvitationNotification> eventObjList =
+                await selectNewFriendInvitation();
+            List<Int64> requestEventIds = [];
+            for (int i = 0; i < eventObjList.length; i++) {
+              if ((eventObjList[i].sender!.id == sender.id &&
+                      eventObjList[i].data!["invitee"] ==
+                          ourchatAppState.thisAccount!.id.toInt()) ||
+                  eventObjList[i].sender!.id ==
+                      ourchatAppState.thisAccount!.id) {
+                eventObjList[i].data!["status"] =
+                    (event.friendInvitationResultNotification.status ==
+                            AcceptFriendInvitationResult
+                                .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
+                        ? 1
+                        : 2);
+                eventObjList[i].read = true;
+                eventObjList[i].data!["result_event_id"] = event.msgId.toInt();
+                requestEventIds.add(eventObjList[i].eventId!);
+                await eventObjList[i].saveToDB(ourchatAppState.privateDB!);
+              }
+            }
+            eventObj = FriendInvitationResultNotification(ourchatAppState,
+                eventId: event.msgId,
+                sender: sender,
+                sendTime: OurchatTime(inputTimestamp: event.time),
+                leaveMessage:
+                    event.friendInvitationResultNotification.leaveMessage,
+                invitee: invitee,
+                accept: (event.friendInvitationResultNotification.status ==
+                        AcceptFriendInvitationResult
+                            .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS
+                    ? true
+                    : false),
+                requestEventIds: requestEventIds);
+            if (event.friendInvitationResultNotification.status ==
+                AcceptFriendInvitationResult
+                    .ACCEPT_FRIEND_INVITATION_RESULT_SUCCESS) {
+              ourchatAppState.thisAccount!.getAccountInfo();
+            }
+            eventObj.read = true;
+
+          case FetchMsgsResponse_RespondEventType.msg:
+            sender.id = event.msg.senderId;
+            List<OneMessage> msgs = [];
+            for (int i = 0; i < event.msg.bundleMsgs.length; i++) {
+              OneMsg oneMsg = event.msg.bundleMsgs[i]; // OneMsg 为grpc对象
+              OneMessage oneMessage = OneMessage(); // OneMessage为OurChat对象
+              if (oneMsg.text.isNotEmpty) {
+                oneMessage.messageType = textMsg;
+                oneMessage.text = oneMsg.text;
+              } else if (oneMsg.image.isNotEmpty) {
+                oneMessage.messageType = imageMsg;
+                oneMessage.imageKey = oneMsg.image;
+              }
+              msgs.add(oneMessage);
+            }
+            eventObj = BundleMsgs(ourchatAppState,
+                eventId: event.msgId,
+                sender: sender,
+                session: OurchatSession(ourchatAppState, event.msg.sessionId),
+                sendTime: OurchatTime(inputTimestamp: event.time),
+                msgs: msgs);
+
+          default:
+            break;
+        }
+        if (eventObj != null) {
+          await eventObj.saveToDB(ourchatAppState.privateDB!);
+          if (listeners.containsKey(eventType)) {
+            // 通知对应listener
+            for (int i = 0; i < listeners[eventType].length; i++) {
+              try {
+                listeners[eventType][i](eventObj);
+              } catch (e) {
+                logger.w("notify listener fail: $e");
+              }
+            }
+          }
+        } else {
+          // event 没有被任何case分支处理，属于未知事件类型
+          logger.w("Unknown event type(id:${event.msgId})");
+        }
+      }
     }
   }
 
