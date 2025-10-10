141552ec1b83ead51131076d4cb83b9ae7f5e595
senlinjun <yu2755221817@163.com>
2025-10-10 20:40:24 +0800

    ci: auto sign for apk

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -1,7 +1,9 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -19,11 +21,28 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    // 配置release签名信息（仅在指定参数时使用）
+    signingConfigs {
+        create("release") {
+            // 从key.properties加载签名信息
+            val keystorePropertiesFile = rootProject.file("app/key.properties")
+            if (keystorePropertiesFile.exists()) {
+                val keystoreProperties = Properties().apply {
+                    load(FileInputStream(keystorePropertiesFile))
+                }
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+                
+                enableV1Signing = true
+                enableV2Signing = true
+            }
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.skyuoi.ourchat"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +51,22 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // 判断是否有签名参数，决定使用哪种签名
+            // 命令行传递 -PuseReleaseSigning=true 时使用release签名
+            val useReleaseSigning = project.hasProperty("useReleaseSigning") 
+                    && project.property("useReleaseSigning") == "true"
+
+            // 默认使用debug签名，指定参数时使用release签名
+            signingConfig = if (useReleaseSigning) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug") // 使用默认的debug签名
+            }
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
@@ -42,3 +74,4 @@ android {
 flutter {
     source = "../.."
 }
+    
